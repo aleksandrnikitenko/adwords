38cd4d3731f0c614de3343697e270e3ae035b26b
Aleksandr Nikitenko <an@branchup.pro>
2018-05-22 17:16:02 +0300

    First commit

---
+++ gears.php
@@ -0,0 +1,89 @@
+<?php 
+	
+
+	class Gear  {
+		
+		public $result = [];
+		public function __construct () {
+				$badSigns = array('+','"',"'",'[',']');
+			$this->getPost($_POST['keywords'],$_POST['keywords1']);
+
+				foreach ($this->preparedData as $key => $value) {						
+					$this->preparedData[$key] = str_replace($badSigns, '', $value);					
+				}
+				foreach ($this->preparedData1 as $key => $value) {						
+					$this->preparedData1[$key] = str_replace($badSigns, '', $value);					
+				}					
+			$this->getMethod($_POST['val']);
+			$this->showResult();		
+		}
+		
+		public function getPost ($incoming,$incoming1) {		   #READY
+			 $this->preparedData = explode("\n", $incoming);
+			 $this->preparedData1 = explode("\n", $incoming1); 		 		
+		}
+		public function getMethod ($postvalue) {				   #READY
+			if($postvalue == 'concat') {
+				$this->concat();
+			} elseif ($postvalue == 'replace') {
+				$this->replace();
+			} elseif ($postvalue == 'wrap') {
+				$this->wrap();
+			} elseif($postvalue == 'filterBy') {
+				$this->filterBy();
+			}
+	}
+		private function showResult () {						   #READY
+				foreach ($this->result as $key => $value) {
+				echo trim($value) . '</br>';
+			} 
+		}
+		public function debug ($arr) {
+
+			echo '<pre>';
+					foreach ($arr as $key => $value) {
+						if ($value) {
+						echo(trim($value)) . "</br>";
+					} else unset ($key);
+				}
+			echo '</pre>';   							#                          #READY
+			
+		}
+		
+		private function filterBy () {
+			// возвращает массив со строками, в которые входит слово из 2-го поля для ввода
+				$pattern = $this->preparedData1[0];
+				$pattern = '#' . trim($pattern) . '#';
+
+			foreach ($this->preparedData as $key => $value) {
+				if(preg_match($pattern, $value)) {
+					array_push($this->result, $value);
+				} else continue;
+					
+							}
+				}							#READY
+		
+	
+		private function replace (/*$preparedData, $pattern*/) {
+				// найти вхождение в строке и заменить на $pattern
+				echo "replace enabled";
+		} 
+		private function concat () {			
+			foreach ($this->preparedData as $value)  {
+					foreach ($this->preparedData1 as $valueone) {
+					array_push($this->result, $value . ' ' . $valueone);							
+				}					
+			} 				#READY
+		}
+
+		private function wrap () {
+			foreach ($this->preparedData as $key => $value) {
+				$value = mb_strtolower($value);				
+				array_push($this->result, '"' . trim($value) . '"');
+
+			}
+		}
+}	
+
+	
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+	include "gears.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="wraper"  >
+		<!-- <h1>Введи келючевики и выбери метод</h1> -->
+			<form action="index.php" method="POST">
+				<textarea type="text" name="keywords"> </textarea> 
+				<textarea type="text" name="keywords1"> </textarea> </br>
+				<button  name='val' value="concat">concat OK</button> <!-- </br> -->
+				<button  name='val' value="replace">replace</button> <!-- </br> -->
+				<button  name='val' value="wrap">wrap OK</button> <!-- </br> -->
+				<button  name='val' value="filterBy">filterBy OK</button> <!-- </br> -->				
+			</form>
+	</div>
+
+	<div class="result">
+		<?php
+			$getData = new Gear;	
+		?>
+	</div>
+</body>
+</html>
